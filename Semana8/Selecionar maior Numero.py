14894143d8c32006e31cd1ba9b4bd2bac4888270
AnonimousX1 <anonimosx1@gmail.com>
2020-05-02 16:15:56 -0300

    Oitava semana

---
+++ Semana8/Selecionar maior Numero.py
@@ -0,0 +1,4 @@
+lista= []
+def maior_elemento(lista):
+    lista.sort(reverse= True)
+    return lista[0]
---
+++ Semana8/Soma de elementos.py
@@ -0,0 +1,7 @@
+lista=[]
+def  soma_elementos(lista):
+    z=0
+    for x in lista:
+        calc = len(lista)
+        z= z+x
+    return(z)
