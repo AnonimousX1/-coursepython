f60a8f19d95d198afcd15bc6ec07c0e44434ca5d
AnonimousX1 <anonimosx1@gmail.com>
2020-05-02 15:46:22 -0300

    Quinta semana, Exercícios opcionais

---
+++ Semana5/desafio/FizzBuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(x):
+    
+def f():
+    if x%3 == 0 :
+        return f
+    if x%5 == 0:
+        return b
+    if x%3 == 0 and x%5== 0:
+        return fb
+    else:
+        return x
